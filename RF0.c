25835524cbcd6409cc7a4dfa4b566f9689d1eb1a
elixios02 <paim@student.42.fr>
2020-11-02 11:46:04 +0100

    prgm 00 01

---
+++ RF0.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#define _GNU_SOURCE
+
+int main(int argc, char **argv)
+{
+	gid_t gid;
+	uid_t uid;
+	char *arg[2];
+
+	if (atoi(argv[1]) == 423)
+	{
+		arg[0] = strdup("/bin/sh");
+		arg[1] = 0;
+		gid = getegid();
+		uid = geteuid();
+		setresgid(gid, gid, gid);
+		setresuid(uid, uid, uid);
+		execv("/bin/sh", arg);
+	}
+	else
+	{
+		fwrite("No !", 1, 5, stderr);
+	}
+	return (0);
+}

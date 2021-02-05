cacbcd01240e09272d87b7a7e5cd5dccb49b31f3
Naminei <nozanne@student.42.fr>
2021-02-05 15:25:26 +0100

    level4

---
+++ level4/source.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int m;
+
+int     n()
+{
+    char    schar[520];
+    fgets(schar,512,stdin);
+    p(schar);
+    if (m == 16930116)
+    {
+        system("/bin/cat /home/user/level5/.pass");
+    }
+    return 0;
+}
+
+int     main(int ac, char **av)
+{
+    n();
+    return (0);
+}

eca9257989351e68278b7248a1ebabfae71f40f7
Naminei <nozanne@student.42.fr>
2021-02-05 15:05:34 +0100

    lvl3

---
+++ level3/source.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int m = 0;
+
+void    v()
+{
+    char    schar[520];
+    fgets(schar,512,stdin);
+    printf("%s\n", schar);
+    if (m == 64)
+    {
+        fwrite("Wait what ?\n", 1, 12, stdout);
+        system("/bin/sh");
+    }
+    return;
+}
+
+int     main()
+{
+    v();
+    return (0);
+}
